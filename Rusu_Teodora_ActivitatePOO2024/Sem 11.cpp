ca02092b15de914750cab546bf200bd81767e0f1
teodo <teodo@TOTO>
2024-12-10 19:15:32 +0200

    Task 2

--- Rusu_Teodora_ActivitatePOO2024/Sem 11.cpp
+++ Rusu_Teodora_ActivitatePOO2024/Sem 11.cpp
@@ -1,109 +1,109 @@
-#include<iostream>
-
-using namespace std;
-
-class Angajat {
-protected:
-	int marca;
-	string nume;
-	float salariu;
-
-public:
-	Angajat() {
-		this->marca = 1234;
-		this->nume = "No name";
-		this->salariu = 3000;
-
-	}
-
-	Angajat(int marca, string nume) {
-		this->marca = marca;
-		this->nume = nume;
-		this->salariu = salariu;
-	}
-
-	virtual float getVenitIncasat() = 0;
-
-	float getSalariu() {
-		return this->salariu;
-	}
-};
-
-class Manager : public Angajat {
-private:
-	int nrSubordonati;
-	float sporPerAngajat;
-
-public:
-	Manager() :Angajat() {
-		//apelam din clasa angajat vnh
-		this->nrSubordonati = 0;
-		this->sporPerAngajat = 0;
-
-
-
-	}
-
-	Manager(int nrSubordonati, float sporPerAngajat) {
-		this->nrSubordonati = nrSubordonati;
-		this->sporPerAngajat = sporPerAngajat;
-	}
-
-	float getSporConducere() {
-		return this->sporPerAngajat;
-
-		
-	}
-	
-};
-
-class ManagerDepartament :public Manager {
-private:
-	string numeDepartament;
-	bool deCercetare;
-
-public:
-	ManagerDepartament():Manager() {
-		//apelam constructorul fara parametrii
-		this->numeDepartament = "Departament";
-		this->deCercetare = false;
-
-	}
-
-	ManagerDepartament(string nume, bool deCercetare) {
-		this->numeDepartament = nume;
-		this->deCercetare = deCercetare;
-	}
-
-	float getVenitIncasat() {
-		float venit = this->salariu;
-		if (this->deCercetare) {
-			venit += (10 / 100) * salariu;
-		}
-		return venit;
-	}
-};
-
-class ManagerDeProiect :public Manager {
-private:
-	string numeProiect;
-	int nrZile;
-	float sporPerZi;
-
-public:
-	ManagerDeProiect(string numeProiect, int nrZile, int nrSubordonati, float sporPerSubordonat) :Manager() {
-		this->numeProiect = numeProiect;
-		this->nrZile = nrZile;
-	}
-
-
-};
-
-int main() {
-	//Angajat a;//nu avem voie sa instantiem o clasa abstracta
-
-
-}
-
-// late biding la momentul executiei
-// earlybiding la momentul compilarii
+//#include<iostream>
+//
+//using namespace std;
+//
+//class Angajat {
+//protected:
+//	int marca;
+//	string nume;
+//	float salariu;
+//
+//public:
+//	Angajat() {
+//		this->marca = 1234;
+//		this->nume = "No name";
+//		this->salariu = 3000;
+//
+//	}
+//
+//	Angajat(int marca, string nume) {
+//		this->marca = marca;
+//		this->nume = nume;
+//		this->salariu = salariu;
+//	}
+//
+//	virtual float getVenitIncasat() = 0;
+//
+//	float getSalariu() {
+//		return this->salariu;
+//	}
+//};
+//
+//class Manager : public Angajat {
+//private:
+//	int nrSubordonati;
+//	float sporPerAngajat;
+//
+//public:
+//	Manager() :Angajat() {
+//		//apelam din clasa angajat vnh
+//		this->nrSubordonati = 0;
+//		this->sporPerAngajat = 0;
+//
+//
+//
+//	}
+//
+//	Manager(int nrSubordonati, float sporPerAngajat) {
+//		this->nrSubordonati = nrSubordonati;
+//		this->sporPerAngajat = sporPerAngajat;
+//	}
+//
+//	float getSporConducere() {
+//		return this->sporPerAngajat;
+//
+//		
+//	}
+//	
+//};
+//
+//class ManagerDepartament :public Manager {
+//private:
+//	string numeDepartament;
+//	bool deCercetare;
+//
+//public:
+//	ManagerDepartament():Manager() {
+//		//apelam constructorul fara parametrii
+//		this->numeDepartament = "Departament";
+//		this->deCercetare = false;
+//
+//	}
+//
+//	ManagerDepartament(string nume, bool deCercetare) {
+//		this->numeDepartament = nume;
+//		this->deCercetare = deCercetare;
+//	}
+//
+//	float getVenitIncasat() {
+//		float venit = this->salariu;
+//		if (this->deCercetare) {
+//			venit += (10 / 100) * salariu;
+//		}
+//		return venit;
+//	}
+//};
+//
+//class ManagerDeProiect :public Manager {
+//private:
+//	string numeProiect;
+//	int nrZile;
+//	float sporPerZi;
+//
+//public:
+//	ManagerDeProiect(string numeProiect, int nrZile, int nrSubordonati, float sporPerSubordonat) :Manager() {
+//		this->numeProiect = numeProiect;
+//		this->nrZile = nrZile;
+//	}
+//
+//
+//};
+//
+//int main() {
+//	//Angajat a;//nu avem voie sa instantiem o clasa abstracta
+//
+//
+//}
+//
+//// late biding la momentul executiei
+//// earlybiding la momentul compilarii
--- Rusu_Teodora_ActivitatePOO2024/Task 2.cpp
+++ Rusu_Teodora_ActivitatePOO2024/Task 2.cpp
@@ -1,80 +1,173 @@
-//#include<iostream>
-//
-//using namespace std;
-//
-//class Palton {
-//public:
-//	string culoare;
-//	bool areNasturi;
-//    string marime;
-//    string material;
-//    int nrPaltoane;
-//    float* pret;
-//    const string producator; 
-//    static int pretMinimPaloane;
-//
-//public:
-//
-//    Palton():producator("Vivo collection") {
-//        this->culoare = "Verde";
-//        this->areNasturi = true;
-//        this->marime = "S";
-//        this->material = "Lana";
-//        this->nrPaltoane = 0;
-//        this->pret = new float[nrPaltoane];
-//        for (int i = 0; i < nrPaltoane; i++) {
-//            this->pret[i] = 100 + i;
-//        }
-//        
-//
-//    }
-//
-//
-//};
-//
-//int Palton::pretMinimPaloane = 0;
-//
-//class Pantof {
-//public:
-//    string culoare;
-//    bool suntDinPiele;
-//    int marime;
-//    int nrPantofi;
-//    float* pret;
-//    const int pretMinPantofi;
-//    static int TVA;
-//
-//    Pantof() :pretMinPantofi(10){
-//        this->culoare = "Bej";
-//        this->suntDinPiele = true;
-//        this->marime = 35;
-//        this->nrPantofi = 0;
-//        this->pret = new float[nrPantofi];
-//        for (int i = 0; i < nrPantofi; i++) {
-//            this->pret[i] = 100 + i;
-//        }
-//    }
-//};
-//
-//int Pantof::TVA = 19;
-//
-//class Camasa {
-//public:
-//    string culoare;
-//    string marime;
-//    int nrCamasi;
-//    char* firma;
-//    const int pretMinCamasa;
-//    static int Aadaos;
-//
-//    Camasa() :pretMinCamasa(70) {
-//        this->culoare = "Bej";
-//        this->marime = "M";
-//        this->nrCamasi = 0;
-//        this->firma = new char[strlen(firma)+1];
-//        strcpy_s(this->firma, strlen(firma) + 1, firma);
-//    }
-//};
-//
-//
-//
+#include<iostream>
+
+using namespace std;
+
+class Palton {
+public:
+	string culoare;
+	bool areNasturi;
+    string marime;
+    string material;
+    int nrPaltoane;
+    float* pret;
+    const string producator; 
+    static int pretMinimPaloane;
+
+public:
+
+    Palton():producator("Vivo collection") {
+        this->culoare = "Verde";
+        this->areNasturi = true;
+        this->marime = "S";
+        this->material = "Lana";
+        this->nrPaltoane = 0;
+        this->pret = new float[nrPaltoane];
+        for (int i = 0; i < nrPaltoane; i++) {
+            this->pret[i] = 100 + i;
+        }
+        
+
+    }
+
+    Palton(string culoare):producator("Vivo collection") {
+        this->culoare = culoare;
+    }
+
+    Palton(string culoare, int nrPaltoane) :producator("Vivo collection") {
+        this->culoare = culoare;
+        this->nrPaltoane = nrPaltoane;
+    }
+
+    Palton(bool areNasturi, int nrPaltoane, float* pret) :producator("Vivo collection") {
+        this->areNasturi = areNasturi;
+        this->nrPaltoane = nrPaltoane;
+        if (this->pret) {
+            delete[]this->pret;
+        }
+        this->pret = new float[nrPaltoane];
+        for (int i = 0; i < nrPaltoane; i++) {
+            this->pret[i] = pret[i];
+        }
+    }
+
+    ~Palton() {
+        if (pret != nullptr) {
+            delete[] pret;
+        }
+    }
+
+
+};
+
+int Palton::pretMinimPaloane = 0;
+
+class Pantof {
+public:
+    string culoare;
+    bool suntDinPiele;
+    int marime;
+    int nrPantofi;
+    float* pret;
+    const int pretMinPantofi;
+    static int TVA;
+
+    Pantof() :pretMinPantofi(10){
+        this->culoare = "Bej";
+        this->suntDinPiele = true;
+        this->marime = 35;
+        this->nrPantofi = 0;
+        this->pret = new float[nrPantofi];
+        for (int i = 0; i < nrPantofi; i++) {
+            this->pret[i] = 100 + i;
+        }
+    }
+
+    Pantof(int marime) :pretMinPantofi(10) {
+        
+        this->marime = marime;
+       
+    }
+
+    Pantof(string culoare,float* pret) :pretMinPantofi(10) {
+        this->culoare = culoare;
+        this->pret = new float[nrPantofi];
+        for (int i = 0; i < nrPantofi; i++) {
+            this->pret[i] = pret[i];
+        }
+    }
+
+    ~Pantof() {
+        if (pret != nullptr) {
+            delete[] pret;
+        }
+    }
+
+};
+
+int Pantof::TVA = 19;
+
+class Camasa {
+public:
+    string culoare;
+    string marime;
+    int nrCamasi;
+    char* firma;
+    const int pretMinCamasa;
+    static int Aadaos;
+
+    Camasa() :pretMinCamasa(70) {
+        this->culoare = "Bej";
+        this->marime = "M";
+        this->nrCamasi = 0;
+        this->firma = new char[strlen(firma)+1];
+        strcpy_s(this->firma, strlen(firma) + 1, firma);
+    }
+
+    Camasa(int nrCamasi) :pretMinCamasa(70) {
+        
+        this->nrCamasi = nrCamasi;
+       
+    }
+
+    Camasa(string culoare, string marime,int nrCamasi,char*firma) :pretMinCamasa(70) {
+        this->culoare = culoare;
+        this->marime = marime;
+        this->nrCamasi = nrCamasi;
+        if(strlen(firma)+1>2){
+            delete[]this->firma;
+        }
+        this->firma = new char[10];
+        strcpy_s(this->firma, 10, firma);
+        
+    }
+
+    ~Camasa() {
+        if (firma != nullptr) {
+            delete[] firma;
+        }
+    }
+};
+
+int main() {
+    Palton p1;
+    Palton("Fucsia");
+    Palton("Gri", 5);
+    float* vector = new float[3] {200, 250, 600};
+    Palton(true, 3, vector);
+    delete[]vector;
+
+    Pantof pantof;
+    Pantof(37);
+    float* vectorPantofi = new float[4] {100, 150, 300, 500};
+    Pantof("Ciclam", vectorPantofi);
+
+    Camasa c;
+    Camasa(45);
+    char* firma=new char[strlen("mamaliga")+1];
+    strcpy_s(firma, strlen("mamaliga") + 1, "mamaliga");
+    Camasa("alba", "S", 20, firma);
+
+
+}
+
+
